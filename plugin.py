d7310f771e8f298e60aa4a2e08831838d8208e9a
Aidan Reilly <aireilly@redhat.com>
2026-02-11 22:18:08 +0000

    yup yupdates
    Signed-off-by: Aidan Reilly <aireilly@redhat.com>
    
    Signed-off-by: Aidan Reilly <aireilly@redhat.com>

--- plugin.py
+++ plugin.py
@@ -1,22 +1,7 @@
-import os
-import shutil
-
 from LSP.plugin import AbstractPlugin, register_plugin, unregister_plugin
 
+
 SESSION_NAME = "mdita-marksman"
-
-
-def _find_binary():
-    """Locate the mdita-marksman binary on the system."""
-    candidates = [
-        os.path.expanduser("~/.local/bin/mdita-marksman"),
-        "/usr/local/bin/mdita-marksman",
-        "/usr/bin/mdita-marksman",
-    ]
-    for path in candidates:
-        if os.path.isfile(path) and os.access(path, os.X_OK):
-            return path
-    return shutil.which("mdita-marksman")
 
 
 class MditaMarksman(AbstractPlugin):
@@ -25,18 +10,8 @@ class MditaMarksman(AbstractPlugin):
         return SESSION_NAME
 
     @classmethod
-    def basedir(cls) -> str:
-        return os.path.join(cls.storage_path(), __package__)
-
-    @classmethod
-    def install_or_update(cls) -> None:
-        binary = _find_binary()
-        if binary is None:
-            raise RuntimeError(
-                "mdita-marksman binary not found. "
-                "Install it from https://github.com/aireilly/mdita-marksman "
-                "and ensure it is available on your PATH or in ~/.local/bin."
-            )
+    def needs_update_or_installation(cls) -> bool:
+        return False
 
 
 def plugin_loaded() -> None:
