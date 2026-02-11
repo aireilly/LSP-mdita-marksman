6cebaf59efd0725effdf27855e1379112d7ada5c
Aidan Reilly <aireilly@redhat.com>
2026-02-11 23:28:34 +0000

    fix
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- plugin.py
+++ plugin.py
@@ -1,4 +1,8 @@
 from LSP.plugin import AbstractPlugin, register_plugin, unregister_plugin
+from LSP.plugin.core.protocol import Location
+from LSP.plugin.core.typing import Any, Callable, List, Mapping
+from LSP.plugin.locationpicker import LocationPicker
+import sublime
 
 
 SESSION_NAME = "mdita-marksman"
@@ -12,6 +16,36 @@ class MditaMarksman(AbstractPlugin):
     @classmethod
     def needs_update_or_installation(cls) -> bool:
         return False
+
+    def on_pre_server_command(self, command: Mapping[str, Any], done_callback: Callable[[], None]) -> bool:
+        command_name = command['command']
+        if command_name == 'marksman.findReferences':
+            command_arguments = command['arguments']
+            if command_arguments and 'locations' in command_arguments[0]:
+                self._handle_show_references(command_arguments[0]['locations'])
+            done_callback()
+            return True
+        return False
+
+    def _handle_show_references(self, references: List[Location]) -> None:
+        session = self.weaksession()
+        if not session:
+            return
+        view = sublime.active_window().active_view()
+        if not view:
+            return
+        if len(references) == 1:
+            args = {
+                'location': references[0],
+                'session_name': session.config.name,
+            }
+            window = view.window()
+            if window:
+                window.run_command('lsp_open_location', args)
+        elif references:
+            LocationPicker(view, session, references, side_by_side=False)
+        else:
+            sublime.status_message('No references found')
 
 
 def plugin_loaded() -> None:
